7bed2ce91d12150123e157e609d88f1ff4ec39e3
Jaret Crist <jaret.crist@gmail.com>
2023-11-24 14:27:51 -0500

    Create initial database

---
+++ table-creation.sql
@@ -0,0 +1,44 @@
+CREATE TABLE sources (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    publisher TEXT,
+    date TEXT,
+    photo_url TEXT
+);
+
+CREATE TABLE spells (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    source INTEGER,
+    school TEXT,
+    casting_time TEXT,
+    range TEXT,
+    components TEXT,
+    duration TEXT,
+    description TEXT,
+    classes TEXT,
+    FOREIGN KEY (source) REFERENCES sources(id)
+);
+
+CREATE TABLE monsters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    type TEXT,
+    actions TEXT,
+    ac TEXT,
+    hp TEXT,
+    spd TEXT,
+    str TEXT,
+    dex TEXT,
+    con TEXT,
+    int TEXT,
+    wis TEXT,
+    cha TEXT,
+    extras TEXT,
+    senses TEXT,
+    languages TEXT,
+    cr TEXT,
+    source INTEGER,
+    image TEXT,
+    FOREIGN KEY (source) REFERENCES sources(id)
+);
